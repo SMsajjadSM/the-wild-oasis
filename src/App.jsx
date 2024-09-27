bef11739933a91c343c897e874e7637822225623
SMsajjadSM <sajjadsiadati1390@gmail.com>
2024-09-27 11:01:23 +0330

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import {QueryClient} from "@tanstack/react-query"
-import GlobalStyles from "./styles/Globalstyles";
+
+import GlobalStyles from "./styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
@@ -10,16 +10,20 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
-const queryClient = new QueryClient({
-  defaultOptions:{
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+ const queryClient = new QueryClient({
+  defaultOptions: {
     queries:{
       staleTime:60*1000
     }
   }
-})
+ });
 function App() {
   return (
     <>
+    <QueryClientProvider client={queryClient}>
+<ReactQueryDevtools initialIsOpen={false}/>
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -37,6 +41,7 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
+    </QueryClientProvider>
     </>
   );
 }
