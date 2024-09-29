f8998368a0d96d15ae42e75e96b646b28dc17ed8
SMsajjadSM <sajjadsiadati1390@gmail.com>
2024-09-29 23:32:57 +0330

    modified:   src/features/cabins/CabinTable.jsx

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,21 +1,53 @@
+import Spinner from "../../ui/Spinner";
+import { useQuery } from "@tanstack/react-query";
+import { getCabins } from "../../services/apiCabins";
+import styled from "styled-components";
+const TableHeader = styled.header`
+  display: grid;
+  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+  column-gap: 2.4rem;
+  align-items: center;
+  background-color: var(--color-grey-50);
+  border-bottom: 1px solid var(--color-grey-100);
+  text-transform: uppercase;
+  letter-spacing: 0.4px;
+  font-weight: 600;
+  color: var(--color-grey-600);
+  padding: 1.6rem 2.4rem;
+`;
+const Table = styled.div`
+  border: 1px solid var(--color-gray-200);
+  font-size: 1.4rem;
+  background-color: var(--color-gray-0);
+  border-radius: 7px;
+  overflow: hidden;
+`;
 
-import Spinner from "../../ui/Spinner"
-import { useQuery } from '@tanstack/react-query';
-import { getCabins } from '../../services/apiCabins';
-
-// v2
 function CabinTable() {
-
-const {isLoading,data:cabin,error} = useQuery({
-  queryKey:['cabin'],queryFn:getCabins,
-});
-if(isLoading)return <Spinner/>
+  const {
+    isLoading,
+    data: cabin,
+    error,
+  } = useQuery({
+    queryKey: ["cabin"],
+    queryFn: getCabins,
+  });
+  if (isLoading) return <Spinner />;
   console.log(cabin);
   console.log(error);
-  
- return <div>
-  dd
- </div>
- 
+
+  return (
+    <Table>
+      <TableHeader>
+        <div> </div>
+        <div> cabin</div>
+        <div> capacity</div>
+        <div> price</div>
+        <div> discount</div>
+        <div> </div>
+        
+      </TableHeader>
+    </Table>
+  );
 }
 export default CabinTable;
