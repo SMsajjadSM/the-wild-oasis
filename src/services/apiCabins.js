060a61222b4f9c5e707464079c07e9e494455920
SMsajjadSM <sajjadsiadati1390@gmail.com>
2024-10-14 22:57:27 +0330

    new file:   src/services/apiCabins.js

---
+++ src/services/apiCabins.js
@@ -0,0 +1,32 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  const { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+}
+export async function delCabin(id) {
+  const { error, data } = await supabase.from("cabins").delete().eq("id", id);
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be deleted");
+  }
+
+  return data;
+}
+export async function insertCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
+    .select();
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be deleted");
+  }
+  return data;
+}
