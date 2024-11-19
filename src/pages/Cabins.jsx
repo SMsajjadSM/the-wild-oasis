345d47e9089bbe24b2e7f0007553a1b677550444
SMsajjadSM <sajjadsiadati1390@gmail.com>
2024-11-19 23:12:33 +0330

    modified:   src/pages/Cabins.jsx

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,14 +1,11 @@
-import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
+import CreateCabinForm from "../features/cabins/CreateCabinForm"
 import CabinTable from "../features/cabins/CabinTable";
-
+import Button from "../ui/Button"
+import { useState } from "react";
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((data) => console.log(data));
-  }, []);
-
+  const [showForm, setShowForm] = useState(false);
   return (
     <>
     <Row type="horizontal">
@@ -18,6 +15,10 @@ function Cabins() {
     <Row>
 
       <CabinTable/>
+      <Button onClick={()=>setShowForm((show)=>!show)}>
+        Add New Cabin
+      </Button>
+      {showForm && <CreateCabinForm/>}
     </Row>
     </>
   );
